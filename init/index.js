c23ffe7ca20b388fb541b245e44d9d200ecd9b0a
monu543 <m20918551@gmail.com>
2026-05-17 18:14:08 +0530

    fixed flash error

--- init/index.js
+++ init/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -13,7 +15,7 @@ main()
   });
 
   async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
   }
 
   const initDB = async () => {
